5b57f2171ef21f8e5676c8e91db80ca8c328dcb7
abreham <aynuyeabresh@gmail.com>
2021-08-19 07:45:17 -0300

    feat: geting data

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -3,22 +3,16 @@ import json
 
 
 data_path = "https://s3-us-west-2-amazonaws.com/usgs-lidar-public"
-#region = 'USGS_LPC_CO_SoPlatteRiver_Lot5_2013_LAS_2015/'
-region= "IA_FullState"
+region = 'USGS_LPC_CO_SoPlatteRiver_Lot5_2013_LAS_2015/'
 bound = "([-93.756155, 41.918015, -93.747334, 41.921429])"
 public_access_path = data_path + region + "ept.json"
 output_flename_laz = "laz/SoPlatteRiver.laz"
 output_flename_tif = "tif/SoPlatteRiver.tif"
-pipline_path = "../get_data.json"
+pipline_path = "get_data.json"
 #region and boundary
 output_flename_laz = "laz/SoPlatteRiver.laz"
-
-def get_raster_terrain(bounds:str,region:str,
-    PUBLIC_ACCESS_PATH:str = public_access_path,
-    OUTPUT_FILENAME_LAZ:str = output_flename_laz,
-    OUTPUT_FILENAME_TIF:str = output_flename_tif,
-    PIPLINE_PATH:str = pipline_path 
-    )->None:
+def get_raster_terrain(bounds:str,region:str, PUBLIC_ACCESS_PATH:str = public_access_path ,OUTPUT_FILENAME_LAZ:str = output_flename_laz,
+OUTPUT_FILENAME_TIF:str = output_flename_tif ,PIPLINE_PATH:str =pipline_path )->None:
 
     with open(PIPLINE_PATH) as json_file:
         the_json = json.load(json_file)
